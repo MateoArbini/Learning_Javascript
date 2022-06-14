3076c757162da25ca409f93dfbd7427bebea8bc2
MateoArbini <mateoarbini1998@gmail.com>
2022-06-14 12:12:51 -0700

    how to declare variables

---
+++ declare_variable.js
@@ -0,0 +1,17 @@
+var myName;
+
+/*This is how you declare a variable.
+In Javascript, there are eight different data types:
+
+-> undefined
+-> null
+-> boolean
+-> string
+-> symbol
+-> bigint
+-> number
+-> object 
+
+Each time you declare a var, it must end with semicolons.
+
+*/
