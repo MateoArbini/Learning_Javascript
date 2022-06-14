86b0f825cd270af33a0c4a9ce973bb437139a37d
MateoArbini <mateoarbini1998@gmail.com>
2022-06-14 13:19:06 -0700

    Quoting Strings with Single Quotes

---
+++ single_quoting_strings.js
@@ -0,0 +1,5 @@
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+/* It is really important to start with one type and end with the same type.
+We can use them, for example, when we want to quotate something inside a string, with the
+use of the backslash*/
