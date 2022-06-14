d157ba0ecd404c2e09ac84c6a44d91d5b33fc5ee
MateoArbini <mateoarbini1998@gmail.com>
2022-06-14 12:35:36 -0700

    understanding case sensitivy in variables

---
+++ case_sensitive.js
@@ -0,0 +1,11 @@
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+/*It is important to mantain the format of the name of the variables*/
