176785ae2640a01046765cdec39e50ca96d614e7
MateoArbini <mateoarbini1998@gmail.com>
2022-06-14 12:45:34 -0700

    Declare a Read-Only Variable with the const Keyword

---
+++ read_only_const_var.js
@@ -0,0 +1,7 @@
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+/* const, has the features that let has, but also this ones are read only.
+They are constant, so it can not be reassigned*/
